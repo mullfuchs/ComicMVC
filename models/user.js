f1a102413a2b8f02987eac4121067323ce3d5a53
mullfuchs <burke.leo@gmail.com>
2017-02-15 14:09:48 -0800

    adding bcrypt

--- models/user.js
+++ models/user.js
@@ -1,10 +1,45 @@
 'use strict';
+var bcrypt = require('bcrypt');
 module.exports = function(sequelize, DataTypes) {
   var user = sequelize.define('user', {
-    email: DataTypes.STRING,
-    name: DataTypes.STRING,
-    password: DataTypes.STRING
-  }, {
+  email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: {
+          msg: 'Invalid email address'
+        }
+      }
+    },
+    name: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [1, 99],
+          msg: 'Name must be between 1 and 99 characters'
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [8, 99],
+          msg: 'Password must be between 8 and 99 characters'
+        }
+      }
+    }
+  }, 
+  hooks: {
+    beforeCreate: function(createdUser, options, cb) {
+      // hash the password
+      var hash = bcrypt.hashSync(createdUser.password, 10);
+      // store the hash as the user's password
+      createdUser.password = hash;
+      // continue to save the user, with no errors
+      cb(null, createdUser);
+    }
+  },
+  {
     classMethods: {
       associate: function(models) {
         models.user.hasMany(models.comic);
