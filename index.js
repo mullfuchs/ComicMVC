ba2a836c816d4e1e3bc13150566845ae1c66ef44
mullfuchs <burke.leo@gmail.com>
2017-02-15 16:03:49 -0800

    added sessions stuff

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 var express = require('express');
 var ejsLayouts = require('express-ejs-layouts');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 var app = express();
+
 
 app.set('view engine', 'ejs');
 
 app.use(require('morgan')('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(ejsLayouts);
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}));
 
 app.get('/', function(req, res) {
   res.render('index');
